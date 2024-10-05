a9d31faa2bf0533051315cb2b7f3651afa2174d0
Isabel Rosário <isabel.rosario102000@gmail.com>
2024-10-05 17:44:22 +0100

    event selector

--- idioms/events.js
+++ idioms/events.js
@@ -1,20 +1,24 @@
 function setupEvents() {
     d3.csv("../../data/economic_events.csv").then(function(data) {
         const uniqueEventNames = [...new Set(data.map(d => d.event_name))];
-        console.log(uniqueEventNames);
-    
+
         const select = d3.select("#event-select");
-        select.append("option")
-            .attr("value", "")
-            .text("Select an event");
-    
+
         uniqueEventNames.forEach(eventName => {
             select.append("option")
                 .attr("value", eventName)
                 .text(eventName);
         });
 
+        function handleEventChange() {
+            const selectedOptions = Array.from(select.node().selectedOptions).map(option => option.value);
+            console.log("Selected Events:", selectedOptions);
+        }
+
+        select.on("change", handleEventChange);
+
     }).catch(function(error) {
         console.error("Error loading the CSV file:", error);
     });
+
 }
